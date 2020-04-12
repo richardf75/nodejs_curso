0f7693644106e5c6626dfb9745f686311a2db01a
Ricardo Florez <richardf75@gmail.com>
2020-04-12 13:16:26 -0500

    en el remoto 2

--- prueba.php
+++ prueba.php
@@ -3,5 +3,5 @@ echo "hola";
  $fecha = date('d-m-Y');
  echo "</br>".$fecha;
  echo "probando esta vaina";
- echo "en este puento estoy en mi repositorio remoto de github"
+ echo "en este puento estoy en mi repositorio remoto de github remoto"
 ?>
